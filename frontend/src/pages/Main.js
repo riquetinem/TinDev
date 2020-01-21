08b02eafb2a753a567dc4b68e6456e7e2868c8c3
Henrique Tinem <henrique714tinem@gmail.com>
2020-01-21 13:02:59 -0300

    frontend

---
+++ frontend/src/pages/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import logo from '../assets/logo.svg';
+
+export default function Main({match}) {
+  return (
+    <div className="main-container">
+      <img src={logo} alt="Tindev"/>
+      <ul>
+        <li>
+           
+        </li>
+      </ul>
+    </div>
+  )
+}
